d8cacc00179cd6879f374fa5402ccd2534faf9a1
SlyMoz <samy.nasset@epitech.eu>
2026-03-01 12:41:24 +0900

    UPDATE: deploy with api secret in github

--- fov-angular/src/environments/environment.example.ts
+++ fov-angular/src/environments/environment.example.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:4000/api',
+  apiUrl: 'http://API_URL_HERE:4000/api',
   twitch: {
     clientId: 'YOUR_CLIENT_ID_HERE',
     clientSecret: 'YOUR_CLIENT_SECRET_HERE'
