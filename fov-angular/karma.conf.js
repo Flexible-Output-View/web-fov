5ec476b0d5c2411caaf52554ad4eaa413d355126
SlyMoz <samy.nasset@epitech.eu>
2026-09-17 10:34:04 +0200

    FIX: angular tests

--- fov-angular/karma.conf.js
+++ fov-angular/karma.conf.js
@@ -13,16 +13,11 @@ module.exports = function (config) {
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
-      jasmine: {
-        // you can add special configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {},
+      clearContext: false,
     },
     jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
+      suppressAll: true,
     },
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage"),
@@ -34,17 +29,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--disable-gpu',
-          '--disable-dev-shm-usage'
-        ]
-      }
-    },
-    browsers: ["ChromeHeadlessCI"],
+    browsers: ["ChromeHeadless"],
     singleRun: false,
     restartOnFileChange: true,
   });
